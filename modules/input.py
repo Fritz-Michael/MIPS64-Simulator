5624a9d8c32173b75182e4e3c18c0fed52ca40ab
Fritz-Michael <fritz_manalili@dlsu.edu.ph>
2018-03-10 15:48:19 +0800

    initial

---
+++ modules/input.py
@@ -0,0 +1,12 @@
+
+class Input:
+
+	def __init__(self):
+		self.instructions = []
+		self.opcodes = []
+
+	def add_instruction(self, instruction):
+		self.instruction.append(instruction)
+
+	def get_opcodes(self):
+		pass
