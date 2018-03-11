e5627cc34bcd9718ef7a82ac10044ff7433d666d
Fritz-Michael <fritz_manalili@dlsu.edu.ph>
2018-03-11 14:32:09 +0800

    cycles

--- modules/internalregisters.py
+++ modules/internalregisters.py
@@ -39,8 +39,46 @@ class WB:
 class InternalRegisters:
 
 	def __init__(self, instructions):
+		self.instructions = instructions
 		self.if_id = IF_ID()
 		self.id_ex = ID_EX()
 		self.ex_mem = EX_MEM()
 		self.mem_wb = MEM_WB()
 		self.wb = WB()
+
+	def cascade_all(self):
+		self.cascade_if_to_id()
+		self.cascade_id_to_ex()
+		self.cascade_ex_to_mem()
+		self.cascade_mem_to_wb()
+
+	def cascade_if_to_id(self):
+		self.id_ex.IR = self.if_id.IR
+		self.id_ex.NPC = self.if_id.NPC
+
+	def cascade_id_to_ex(self):
+		self.ex_mem.IR = self.id_ex.IR
+
+	def cascade_ex_to_mem(self):
+		self.mem_wb.IR = self.ex_mem.IR
+		self.mem_wb.ALU = self.ex_mem.ALU
+
+	def cascade_mem_to_wb(self):
+		self.wb.IR = self.mem_wb.IR
+
+	def instruction_fetch(self):
+		self.if_id.IR = self.instructions
+		self.if_id.PC += 4
+		self.if_id.NPC += 4
+
+	def instruction_decode(self):
+		pass
+
+	def execution(self):
+		pass
+
+	def memory_access(self):
+		pass
+
+	def writeback(self):
+		pass
--- modules/pipelinemap.py
+++ modules/pipelinemap.py
@@ -1,8 +1,33 @@
+from internalregisters import *
 
 class Pipeline:
 
 	def __init__(self, instructions):
 		self.instructions = instructions
+		self.internal_registers = InternalRegisters(instructions)
+		self.cycles = []
 
 	def get_pipeline(self):
+		end = False
+		cycle = [None,None,None,None,None]
+		while(!end):
+			self.internal_registers.cascade_all()
+
+			if self.internal_registers.wb.IR != 0:
+				self.internal_registers.writeback()
+				cycle[4] = 'WB'
+				pass
+			if self.internal_registers.mem_wb.IR != 0:
+				self.internal_registers.memory_access()
+				cycle[3] = 'MEM'
+			if self.internal_registers.ex_mem.IR != 0:
+				self.internal_registers.execution()
+				cycle[2] = 'EX'
+			if self.internal_registers.id_ex.IR != 0:
+				self.internal_registers.instruction_decode()
+				cycle[1] = 'ID'
 		
+			self.internal_registers.instruction_fetch()
+			cycle[0] = 'IF'
+
+
