09a2167ba8b6f12fd48e3a2646b15e31bd22c95c
Fritz-Michael <fritz_manalili@dlsu.edu.ph>
2018-03-18 16:25:07 +0800

    started executiion cycle

--- modules/internalregisters.py
+++ modules/internalregisters.py
@@ -1,3 +1,4 @@
+from registers import *
 
 class IF_ID:
 	
@@ -69,29 +70,60 @@ class InternalRegisters:
 	def instruction_fetch(self):
 		if self.if_id.PC/4 > len(self.instructions)-1:
 			self.if_id.IR = 0
+			return False
 		else:
 			self.if_id.IR = self.instructions[int(self.if_id.PC/4)]
 			self.if_id.PC += 4
 			self.if_id.NPC += 4
+			return True
 
 	def instruction_decode(self):
-		#self.cascade_if_to_id()
-		self.id_ex.A = hex(int(bin(int(self.if_id.IR,16))[2:].zfill(32)[6:11],2))[2:].zfill(16).upper()
-		self.id_ex.B = hex(int(bin(int(self.if_id.IR,16))[2:].zfill(32)[11:16],2))[2:].zfill(16).upper()
-		self.id_ex.IMM = hex(int(bin(int(self.if_id.IR,16))[2:].zfill(32)[16:],2))[2:].zfill(16).upper()
+		if self.if_id.IR != 0:
+			self.id_ex.A = hex(int(bin(int(self.if_id.IR,16))[2:].zfill(32)[6:11],2))[2:].zfill(16).upper()
+			self.id_ex.B = hex(int(bin(int(self.if_id.IR,16))[2:].zfill(32)[11:16],2))[2:].zfill(16).upper()
+			self.id_ex.IMM = hex(int(bin(int(self.if_id.IR,16))[2:].zfill(32)[16:],2))[2:].zfill(16).upper()
+			self.cascade_if_to_id()
+			return True
+		else:
+			self.id_ex.IR = 0
+			return False
 
 	def execution(self):
-		if self.ex_mem.IR[0:7] == '': #register-to-register
-			pass
-		elif self.ex_mem.IR[0:7] == '011001' or self.ex_mem.IR[0:7] == '001110': #register-to-immediate
-			pass
-		elif self.ex_mem.IR[0:7] == '': #shift
-			pass
-		elif self.ex_mem.IR[0:7] == '110111' or self.ex_mem.IR[0:7] == '111111': #memory reference
-			pass
+		if self.id_ex.IR != 0:
+			self.temp_ir = bin(int(self.id_ex.IR,16))[2:].zfill(32)
+			if self.temp_ir[26:32] == '101101': #register-to-register
+				self.ex_mem.ALU = int(self.id_ex.A,16) + int(self.id_ex.B,16)
+				print(self.ex_mem.ALU)
+			elif self.temp_ir[0:6] == '011001' or self.temp_ir[0:7] == '001110': #register-to-immediate
+				self.ex_mem.ALU = int(self.id_ex.A,16) + int(self.id_ex.IMM,16)
+				print('pass reg to imm')
+			elif self.temp_ir[26:32] == '101010': #set
+				if int(self.id_ex.A,16) < int(self.id_ex.B,16):
+					self.ex_mem.ALU = 1
+				else:
+					self.ex_mem.ALU = 0
+				print('pass set')
+			elif self.temp_ir[0:6] == '110111' or self.temp_ir[0:7] == '111111': #memory reference
+				self.ex_mem.ALU = int(self.id_ex.A,16) + int(self.id_ex.IMM,16)
+				print('pass memory reference')
+			self.cascade_id_to_ex()
+			return True
+		else:
+			self.ex_mem.IR = 0
+			return False
 
 	def memory_access(self):
-		pass
+		if self.ex_mem.IR != 0:
+			self.cascade_ex_to_mem()
+			return True
+		else:
+			self.mem_wb.IR = 0
+			return False
 
 	def writeback(self):
-		pass
+		if self.mem_wb.IR != 0:
+			self.cascade_mem_to_wb()
+			return True
+		else:
+			self.wb.IR = 0
+			return False
--- modules/pipelinemap.py
+++ modules/pipelinemap.py
@@ -12,33 +12,22 @@ class Pipeline:
 
 		while not end:
 			cycle = [None,None,None,None,None]
-			#self.internal_registers.cascade_all()
 
-			self.internal_registers.cascade_mem_to_wb()
-			if self.internal_registers.wb.IR != 0:
-				self.internal_registers.writeback()
+			if self.internal_registers.writeback():
 				cycle[4] = 'WB'
-
 				if self.internal_registers.wb.IR == self.instructions[-1]:
 					end = True
 
-			self.internal_registers.cascade_ex_to_mem()
-			if self.internal_registers.mem_wb.IR != 0:
-				self.internal_registers.memory_access()
+			if self.internal_registers.memory_access():
 				cycle[3] = 'MEM'
 
-			self.internal_registers.cascade_id_to_ex()
-			if self.internal_registers.ex_mem.IR != 0:
-				self.internal_registers.execution()
+			if self.internal_registers.execution():
 				cycle[2] = 'EX'
 
-			self.internal_registers.cascade_if_to_id()
-			if self.internal_registers.id_ex.IR != 0:
-				self.internal_registers.instruction_decode()
+			if self.internal_registers.instruction_decode():
 				cycle[1] = 'ID'
-		
-			self.internal_registers.instruction_fetch()
-			if self.internal_registers.if_id.IR != 0:
+
+			if self.internal_registers.instruction_fetch():
 				cycle[0] = 'IF'
 
 			self.cycles.append(cycle)
@@ -47,8 +36,7 @@ class Pipeline:
 
 if __name__ == '__main__':
 	instructions = ['0043082D']
+	# a = hex(int(bin(int(instructions[0],16))[2:].zfill(32)[6:11],2))[2:].zfill(16).upper()
+	# print(int(a,16))
 	temp = Pipeline(instructions)
 	temp.get_pipeline()
-	# print(hex(int(bin(int(instructions[0],16))[2:].zfill(32)[6:11],2))[2:].zfill(16))
-	# print(bin(int(instructions[0],16))[2:].zfill(32)[11:16])
-	# print(bin(int(instructions[0],16))[2:].zfill(32)[16:])
